a438d85093b69e2d5b684166727f3f3d3b028d3c
nahxx <goodhik8@daum.net>
2022-05-30 17:02:44 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -1,7 +1,3 @@
 $(function() {
-  // top-nav 클릭이벤트 (open 클래스 추가)
-  $('.gnb1 > li').click(function() {
-    $('.gnb2').not($('.gnb2', this)).removeClass('open');
-    $('.gnb2', this).toggleClass('open');
-  })
+  
 });
