e85e707a6ef317646e98933c42cc4bd161060eb0
nahxx <goodhik8@daum.net>
2022-05-30 21:30:46 +0900

    메인 푸터 구현
    메인 푸터 구현

--- js/common.js
+++ js/common.js
@@ -4,4 +4,15 @@ $(function() {
    $('.gnb2').not($('.gnb2', this)).removeClass('open');
    $('.gnb2', this).toggleClass('open');
  });
+
+ // footer01 button 클릭이벤트 (open 클래스 추가)
+ $('.footer01 .select').on('click', 'button', function() {
+   $('.footer01 .select').not($(this).parent()).removeClass('open');
+   $(this).parent().toggleClass('open');
+ });
+
+ $('.top-btn a').click(function() {
+   $('html, body').animate({scrollTop: 0}, 1500);
+   return false;
+ });
 });
